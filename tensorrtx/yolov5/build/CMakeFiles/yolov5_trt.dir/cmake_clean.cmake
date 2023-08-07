ea2235ef1db8136c2cd8a2c7b65ea6bc135dd686
zhanghaichao <zhc22707@dingtalk.com>
2023-08-07 09:30:53 +0800

    detect_and_deploy

---
+++ tensorrtx/yolov5/build/CMakeFiles/yolov5_trt.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yolov5_trt.dir/calibrator.cpp.o"
+  "CMakeFiles/yolov5_trt.dir/yolov5_trt.cpp.o"
+  "yolov5_trt"
+  "yolov5_trt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yolov5_trt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
